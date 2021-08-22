c6993dd88905c3f07a6653e218f1b08d70d97075
Andy Jhoao <andy_joao@live.com>
2021-08-22 14:31:20 -0500

    config front

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   parserOptions: {
     parser: "babel-eslint",
   },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
