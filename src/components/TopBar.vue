8f0818335e5d559c26cab833876244e94cb18efa
Andy Jhoao <andy_joao@live.com>
2021-08-22 15:11:39 -0500

    new front

---
+++ src/components/TopBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="topbar">
+      <a class="back" v-if="back" @click="$router.back()">
+        ⬅ Volver
+      </a>
+      <span class="title">
+        <slot />
+      </span>
+      <span class="userProps">
+        {{ user }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopBar',
+  props: {
+    back: {
+      type: Boolean,
+      default: false,
+    },
+    user: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.topbar {
+  display: flex;
+  align-items: center;
+  background-color: #638bff;
+  justify-content: space-between;
+  color: #fff;
+  height: 42px;
+  text-transform: uppercase;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+.topbar > * {
+  padding-left: 16px;
+}
+
+.topbar-spacing {
+  height: 42px;
+  width: 100%;
+}
+.back {
+  cursor: pointer;
+}
+.userProps {
+  margin-right: 16px;
+}
+</style>
---
+++ src/components/UserList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="chat-list">
+    <h3>Usuarios</h3>
+
+    <user-list-item
+      class="selectUser"
+      v-for="user in $store.state.user.list"
+      :key="user._id"
+      :id="user._id"
+      :name="user.name"
+    />
+  </div>
+</template>
+
+<script>
+import UserListItem from '@/components/UserListItem.vue';
+
+export default {
+  name: 'ChatList',
+  components: {
+    UserListItem,
+  },
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  created() {
+    this.$store.dispatch('user/getUsers');
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  text-align: left;
+  padding-left: 16px;
+}
+.selectUser {
+  cursor: pointer;
+}
+.selectUser:hover {
+  background-color: #f1f1f1;
+}
+.chat-list {
+  margin-top: 50px;
+}
+</style>
